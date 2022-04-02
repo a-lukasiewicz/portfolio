b587f9c46995fe1b88a110668152778e18e0499b
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2022-04-02 10:33:52 +0200

    finished Navbar, Hero components

---
+++ src/components/Hero.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <div class="h-[92vh] bg-blue-500 text-white relative">
+        <div class="flex flex-col items-center justify-center h-full">
+            <h1 class="text-4xl">Adam Łukasiewicz</h1>
+            <p class="text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br />
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <div class="absolute text-black right-20 bottom-10">
+                <div class="flex">
+                    <img src="../../github.svg" />
+                    <img src="../../email.svg" />
+                    <img src="../../linkedin.svg" />
+                </div>
+            </div>
+            <div class="absolute bottom-40">
+                <button class="nav-link bg-sky-400 hover:bg-sky-300">Przejdź dalej</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
