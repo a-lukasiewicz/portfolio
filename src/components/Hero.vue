8f2ec9bc86bc3f775b43a3ca09b9c435d234d4e1
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2022-04-07 16:04:14 +0200

    add svgs, change hero footer table components

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,11 +1,16 @@
 <template lang="">
   <div class="h-[48vh] lg:h-[92vh] bg-mainLighter text-white relative">
-    <div class="flex flex-col items-center justify-center h-full">
-      <h1 class="text-4xl">Adam Łukasiewicz</h1>
-      <p class="p-3 text-center">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br />
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-      </p>
+    <div class="flex items-start justify-around h-full">
+      <div class="flex flex-col items-center justify-center h-full">
+        <h1 class="text-4xl">Adam Łukasiewicz</h1>
+        <p class="p-3 text-xl italic text-center">
+          Web Developer <br />
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, ullam.
+        </p>
+      </div>
+      <div class="py-8">
+        <img src="../../public/hero.svg" alt="">
+      </div>
       <div class="absolute text-black bottom-1 right-5 lg:right-20 lg:bottom-10">
         <div class="flex">
           <img class="px-2" src="../../public/github.svg" />
@@ -13,7 +18,7 @@
           <img class="px-2" src="../../public/email.svg" />
         </div>
       </div>
-      <div class="absolute bottom-10 lg:bottom-40">
+      <div class="absolute bottom-10 lg:bottom-20">
         <button class="nav-link bg-sky-400 hover:bg-sky-300">Przejdź dalej</button>
       </div>
     </div>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -7,35 +7,59 @@
     </thead>
     <tbody>
       <tr class="hover">
-        <td>VUE 3</td>
+        <td class="flex items-center">
+          <img src="../../public/technology/vue.svg" class="w-8 h-8" />
+          <p class="px-3">VUE 3</p>
+        </td>
       </tr>
       <tr class="hover">
-        <td>JAVASCRIPT ES6</td>
+        <td class="flex items-center">
+          <img src="../../public/technology/js.svg" class="w-8 h-8" />
+          <p class="px-3">JAVASCRIPT ES6</p>
+        </td>
       </tr>
       <tr class="hover">
-        <td>TAILWIND CSS</td>
+        <td class="flex items-center">
+          <img src="../../public/technology/tailwind.svg" class="w-8 h-8" />
+          <p class="px-3">TAILWIND CSS</p>
+        </td>
       </tr>
       <tr class="hover">
-        <td>HTML5 + CSS3</td>
+        <td class="flex items-center">
+          <img src="../../public/technology/html.svg" class="w-8 h-8" />
+          <img src="../../public/technology/css.svg" class="w-8 h-8" />
+          <p class="px-3">HTML5 + CSS3</p>
+        </td>
       </tr>
       <tr class="hover">
-        <td>PYTHON</td>
+        <td class="flex items-center">
+          <img src="../../public/technology/python.svg" class="w-8 h-8" />
+          <p class="px-3">PYTHON</p>
+        </td>
       </tr>
       <tr class="hover">
-        <td>VS CODE</td>
+        <td class="flex items-center">
+          <img src="../../public/technology/vscode.svg" class="w-8 h-8" />
+          <p class="px-3">VS CODE</p>
+        </td>
       </tr>
       <tr class="hover">
-        <td>GITHUB</td>
+        <td class="flex items-center">
+          <img src="../../public/technology/github.svg" class="w-8 h-8" />
+          <p class="px-3">GITHUB</p>
+        </td>
       </tr>
       <tr class="hover">
-        <td>
-          TYPESCRIPT
+        <td class="flex items-center">
+          <img src="../../public/technology/typescript.svg" class="w-8 h-8" />
+          <p class="px-3">TYPESCRIPT</p>
           <span class="p-3 text-white badge badge-ghost badge-sm bg-mainLighter">Aktualnie się uczę</span>
         </td>
       </tr>
       <tr class="hover">
-        <td>
-          REACT
+        <td class="flex items-center">
+          <img src="../../public/technology/react.svg" class="w-8 h-8" />
+          <p class="px-3">REACT</p>
           <span class="p-3 text-white badge badge-ghost badge-sm bg-mainLighter">Aktualnie się uczę</span>
         </td>
       </tr>
