a19786fddd3886c680c7e096aed5b6bc4f877f57
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2022-04-13 16:47:25 +0200

    remove unused libary

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
-import VueObserveVisibility from "vue-observe-visibility";
 import "./assets/index.css";
 import "animate.css";
 
-createApp(App).use(VueObserveVisibility).use(router).mount("#app");
+createApp(App).use(router).mount("#app");
